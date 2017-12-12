1e7f586e03ac4684bf10995b6fd605ad237d61a6
emersondelonga <emersondelonga@gmail.com>
2017-12-12 11:53:05 -0500

    Electoral Map First Commit

---
+++ ElectoralMap.java
@@ -0,0 +1,10 @@
+import java.awt.Color;
+public class ElectoralMap{
+    private class subRegion{
+        String name;
+        double[] xcoords;
+        double[] ycoords;
+        int[] votes;
+        Color color;
+    }
+}
